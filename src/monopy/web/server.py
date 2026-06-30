5a72188dd398deda7a656debb751c53991c60e25
dtkvn <dtkvn@pm.me>
2026-06-30 22:49:03 +0200

    stuff

--- src/monopy/web/server.py
+++ src/monopy/web/server.py
@@ -299,7 +299,7 @@ app.mount("/static", StaticFiles(directory=str(STATIC_DIR)), name="static")
 
 
 def main():
-    uvicorn.run("monopy.web.server:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("monopy.web.server:app", host="0.0.0.0", port=1997, reload=True)
 
 
 if __name__ == "__main__":
